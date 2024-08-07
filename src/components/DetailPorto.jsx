b19d95253ec83035f5e309318d0867560d363927
Nur Khairiyah <96655412+nrkay@users.noreply.github.com>
2024-08-07 21:31:45 +0700

    finsh porto

--- src/components/DetailPorto.jsx
+++ src/components/DetailPorto.jsx
@@ -1,11 +1,74 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { CiLink } from "react-icons/ci";
+import { ColorRing } from "react-loader-spinner";
+import { db } from "../configuration";
+import { useNavigate } from "react-router-dom";
+
 function DetailPorto({ id }) {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const detailArticle = doc(db, "portofolios", id);
+            const docSnap = await getDoc(detailArticle);
+            setData(docSnap.data())
+        } catch (error) {
+            setMessage(error.message);
+            console.log("error terjadi")
+        }
+        setIsLoading(false)
+    }
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    console.log(data)
     return (
         <>
-        <div className="grid grid-cols-3 lg:grid-cols-3">
-            <div className="">p</div>
-            <div className="lg:col-span-2">e</div>
-        </div>
-            <p>{ id }</p>
+            {isLoading ? (
+                <div className=" w-full h-96 flex justify-center items-center">
+                    <ColorRing
+                        visible={true}
+                        height="80"
+                        width="80"
+                        ariaLabel="color-ring-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="color-ring-wrapper"
+                        colors={['#01204e', '#1A365F', '#334C71', '#667994', '#CCD2DB']}
+                    />
+                </div>
+            ) : (
+                <>
+                   
+                    <div className="mt-2 grid grid-cols-1 lg:grid-cols-3">
+                        <div className="">
+                            <img
+                                className="object-contain h-40 w-full"
+                                src={data.image || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'}
+                                alt='not-available'
+                            />
+                        </div>
+                        <div className="detail-work px-5 lg:col-span-2 pt-2 lg:pt-0">
+                            <p className="text-base lg:text-lg font-light">{data.Subject || ''}</p>
+                            <h1 className="text-xl lg:text-2xl font-bold">{data.Title || ''}</h1>
+                        </div>
+                    </div>
+                    <div className="mt-6">
+                        <p>{data.Desc}</p>
+                    </div>
+                    <div className="mt-5 flex">
+                        <CiLink />
+                        <p className="ml-2 font-bold text-base text-zinc-700">Link :</p>
+                        <p className="ml-2 font-normal text-sky-600">{data.Link || '-'}</p>
+                    </div>
+
+                </>
+            )}
+
 
         </>
     );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,6 +7,8 @@ function Work({ activeDetail }) {
     const [article, setArticle] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [message, setMessage] = useState("");
+    const [type, setType] = useState("portofolio");
+
     const fetchData = async () => {
         setIsLoading(true)
         try {
@@ -45,32 +47,33 @@ function Work({ activeDetail }) {
                     {article && article.length > 0 ? (
                         <div data-aos="fade-left" className="">
                             <div className="title-desc flex items-center">
-                                <h1>Portofolio</h1>
+                                <h1>Portofolio & Certificate</h1>
                                 <div className="line-title w-1/3 h-1 ml-3 lg:ml-8 rounded-full"></div>
                             </div>
                             <div className="flex title-working ">
-                                <p className="mr-2">All</p>
-                                <p className="mr-2">Front-end Development</p>
+                                <p onClick={(() => { setType("portofolio") })} className="mr-2 text-zinc-700 cursor-pointer hover:text-sky-600">Portofolio</p>
+                                <p onClick={(() => { setType("certificate") })} className="mr-2 text-zinc-700 cursor-pointer hover:text-sky-600">Certificate</p>
                             </div>
                             <div className="mt-3 grid grid-cols-2 lg:grid-cols-3 gap-2">
-                                {article.map((item) => (
-                                    <button onClick={()=> activeDetail(item.id)}>
-                                        <div key={item.id} className="p-1 bg-[#F2F5F9] rounded text-left">
-                                            <div className="rounded bg-gray-50 px-2">
-                                                <img
-                                                    className="object-contain h-40 w-full"
-                                                    src={item.image || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'} // Ganti dengan URL gambar default jika perlu
-                                                    alt='not-available'
-                                                />
+                                {article
+                                    .filter(item => item.type === type) // Filter items based on the current type
+                                    .map(item => (
+                                        <button key={item.id} onClick={() => activeDetail(item.id)}>
+                                            <div className="p-1 bg-[#F2F5F9] rounded text-left">
+                                                <div className="rounded bg-gray-50 px-2">
+                                                    <img
+                                                        className="object-contain h-40 w-full"
+                                                        src={item.image || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'}
+                                                        alt='not-available'
+                                                    />
+                                                </div>
+                                                <div className="pt-2">
+                                                    <p className="section-title-work text-xs ">{item.Subject || 'No description'}</p>
+                                                    <p className="truncate text-sm lg:text-base font-medium">{item.Title || 'No title'}</p>
+                                                </div>
                                             </div>
-                                            <div className="pt-2">
-                                                <p className="section-title-work text-xs ">{item.Subject || 'No description'}</p>
-                                                <p className="truncate text-sm lg:text-base font-medium">{item.Title || 'No title'}</p>
-                                            </div>
-                                        </div>
-                                    </button>
-
-                                ))}
+                                        </button>
+                                    ))}
                             </div>
                         </div>
                     ) : (
