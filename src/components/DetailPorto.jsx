b6bdd63f7c3d9339c9d77772c311ec95bf3463b3
Nur Khairiyah <96655412+nrkay@users.noreply.github.com>
2024-09-24 14:59:33 +0700

    fix detailProject

--- src/components/DetailPorto.jsx
+++ src/components/DetailPorto.jsx
@@ -1,13 +1,20 @@
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { CiLink } from "react-icons/ci";
 import { ColorRing } from "react-loader-spinner";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { db } from "../configuration";
-import { useNavigate } from "react-router-dom";
 
 function DetailPorto({ id }) {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    const [images, setImages] = useState([]);
     const [message, setMessage] = useState("");
 
     const fetchData = async () => {
@@ -15,22 +22,34 @@ function DetailPorto({ id }) {
         try {
             const detailArticle = doc(db, "portofolios", id);
             const docSnap = await getDoc(detailArticle);
+            const articleData = docSnap.data();
             setData(docSnap.data())
+            // if (articleData?.type == "certificate") {
+                const imagesRef = collection(db, `portofolios/${id}/image`);
+                const imagesSnapshot = await getDocs(imagesRef);
+                const images = imagesSnapshot.docs.map(doc => doc.data());
+                setImages(images);
+            // }
+            setIsLoading(false)
         } catch (error) {
             setMessage(error.message);
-            console.log("error terjadi")
+            setIsLoading(false)
         }
-        setIsLoading(false)
     }
+
+
     useEffect(() => {
         fetchData();
     }, [])
 
-    console.log(data)
+    console.log("ini data", images)
+
+
     return (
+
         <>
             {isLoading ? (
-                <div className=" w-full h-96 flex justify-center items-center">
+                <div className="w-full h-96 flex justify-center items-center">
                     <ColorRing
                         visible={true}
                         height="80"
@@ -43,33 +62,115 @@ function DetailPorto({ id }) {
                 </div>
             ) : (
                 <>
-                   
-                    <div className="mt-2 grid grid-cols-1 lg:grid-cols-3">
-                        <div className="">
-                            <img
-                                className="object-contain h-40 w-full"
-                                src={data.image || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'}
-                                alt='not-available'
-                            />
-                        </div>
-                        <div className="detail-work px-5 lg:col-span-2 pt-2 lg:pt-0">
-                            <p className="text-base lg:text-lg font-light">{data.Subject || ''}</p>
-                            <h1 className="text-xl lg:text-2xl font-bold">{data.Title || ''}</h1>
-                        </div>
-                    </div>
-                    <div className="mt-6 px-5" >
-                        <p>{data.Desc}</p>
-                    </div>
-                    <div className="mt-5 flex">
-                        <CiLink />
-                        <p className="ml-2 font-bold text-base text-zinc-700">Link :</p>
-                        <p className="ml-2 font-normal text-sky-600">{data.Link || '-'}</p>
-                    </div>
+                    {data.type === 'certificate' ? (
+                        <>
+                            <h1 className="text-xl lg:text-2xl font-bold mb-3 text-center">{data.Title || ''}</h1>
+                            <Swiper
+                                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                spaceBetween={50}
+                                slidesPerView={1}
+                                navigation
+                                pagination={{ clickable: true }}
+                            >
+                                {images && images.length > 0 ? (
+                                    <>
+                                        {
+                                            images.map(item => (
+                                                <>
+                                                    <SwiperSlide key={item.name}>
+                                                        <div className="col-span-2 flex justify-center mb-5 ">
+                                                            <img
+                                                                className="h-auto max-w-80 lg:max-w-lg rounded-lg"
+                                                                src={item.name || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'}
+                                                                alt="description certificate"
+                                                            />
+                                                        </div>
+                                                    </SwiperSlide>
+                                                </>
+                                            ))
+                                        }
+                                    </>
+                                ) : (
+                                    <SwiperSlide>
+                                        <div className="col-span-2 flex justify-center mb-5">
+                                            <img className="h-auto max-w-80 lg:max-w-lg rounded-lg"
+                                                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'
+                                                alt="description empty"
+                                            />
+                                        </div>
+                                    </SwiperSlide>
+                                )}
+                            </Swiper>
 
+                        </>
+                    ) : (
+                        <>
+                            <div className="mt-2 grid grid-cols-1 lg:grid-cols-3">
+                                <div className="">
+                                    <img
+                                        className="object-contain h-40 w-full"
+                                        src={data.image || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'}
+                                        alt="not-available"
+                                    />
+                                </div>
+                                <div className="detail-work px-5 lg:col-span-2 pt-2 lg:pt-0">
+                                    <p className="text-base lg:text-lg font-light">{data.Subject || ''}</p>
+                                    <h1 className="text-xl lg:text-2xl font-bold">{data.Title || ''}</h1>
+                                </div>
+                            </div>
+                            <div className="mt-6 px-5">
+                                <p>{data.Desc}</p>
+                            </div>
+                            <div className="mt-5 flex">
+                                <CiLink />
+                                <p className="ml-2 font-bold text-base text-zinc-700">Link :</p>
+                                <p className="ml-2 font-normal text-sky-600">{data.Link || '-'}</p>
+                            </div>
+                            <div className="mt-8">
+                                <>
+                                    <Swiper
+                                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                        spaceBetween={50}
+                                        slidesPerView={1}
+                                        navigation
+                                        pagination={{ clickable: true }}
+                                    >
+                                        {images && images.length > 0 ? (
+                                            <>
+                                                {
+                                                    images.map(item => (
+                                                        <>
+                                                            <SwiperSlide key={item.name}>
+                                                                <div className="col-span-2 flex justify-center mb-5 ">
+                                                                    <img
+                                                                        className="h-auto max-w-80 lg:max-w-lg rounded-lg"
+                                                                        src={item.name || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'}
+                                                                        alt="description certificate"
+                                                                    />
+                                                                </div>
+                                                            </SwiperSlide>
+                                                        </>
+                                                    ))
+                                                }
+                                            </>
+                                        ) : (
+                                            <SwiperSlide>
+                                                <div className="col-span-2 flex justify-center mb-5">
+                                                    <img className="h-auto max-w-80 lg:max-w-lg rounded-lg"
+                                                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuT_ZQntoxW24XtuoMjO7vp_VwX3na6oY8Dg&s'
+                                                        alt="description empty"
+                                                    />
+                                                </div>
+                                            </SwiperSlide>
+                                        )}
+                                    </Swiper>
+
+                                </>
+                            </div>
+                        </>
+                    )}
                 </>
             )}
-
-
         </>
     );
 }
