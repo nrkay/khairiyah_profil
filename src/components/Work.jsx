7d5100d1c82362537ee1c541025024ec60ff25d1
Nur Khairiyah <96655412+nrkay@users.noreply.github.com>
2024-12-27 13:21:08 +0700

    delete fiture work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -50,13 +50,13 @@ function Work({ activeDetail }) {
                                 <h1>Portofolio & Certificate</h1>
                                 <div className="line-title w-1/3 h-1 ml-3 lg:ml-8 rounded-full"></div>
                             </div>
-                            <div className="flex title-working ">
+                            {/* <div className="flex title-working ">
                                 <p onClick={(() => { setType("portofolio") })} className="mr-2 text-zinc-700 cursor-pointer hover:text-sky-600">Portofolio</p>
                                 <p onClick={(() => { setType("certificate") })} className="mr-2 text-zinc-700 cursor-pointer hover:text-sky-600">Certificate</p>
-                            </div>
+                            </div> */}
                             <div className="mt-3 grid grid-cols-2 lg:grid-cols-3 gap-2">
                                 {article
-                                    .filter(item => item.type === type) // Filter items based on the current type
+                                   // Filter items based on the current type
                                     .map(item => (
                                         <button key={item.id} onClick={() => activeDetail(item.id)}>
                                             <div className="p-1 bg-[#F2F5F9] rounded text-left">
